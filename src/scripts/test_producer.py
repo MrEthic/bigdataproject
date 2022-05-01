b233c57c3ce716d7a097ecc848c74b3c38dec4ea
MrEthic <jeremiebasso@hotmail.fr>
2022-05-01 19:09:05 +0200

    missing token

--- src/scripts/test_producer.py
+++ src/scripts/test_producer.py
@@ -4,7 +4,7 @@ import os
 #python -m src.scripts.test_producer
 
 if __name__ == '__main__':
-    stream = TweetProducer(os.environ['TWITTER_BEARER'], wait_on_rate_limit=True)
+    stream = TweetProducer(bearer_token=os.environ['TWITTER_BEARER'], wait_on_rate_limit=True)
     try:
         stream.start()
     except KeyboardInterrupt:
