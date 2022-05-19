12e1b097fe9284a805490e5b64d22c668ec0cda1
MrEthic <jeremiebasso@hotmail.fr>
2022-05-19 14:35:11 +0200

    formating v0.1

--- src/spark/formating.py
+++ src/spark/formating.py
@@ -2,16 +2,37 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode
 
 # Create SparkSession
-spark = SparkSession.builder.master("local[1]").getOrCreate()
+spark = SparkSession.builder.appName('TEST').\
+    master('local[1]').\
+    config("spark.mongodb.input.uri", "mongodb+srv://remote_worker:remote_worker@bddbd.ptwl0.mongodb.net/bigdataproject").\
+    config("spark.mongodb.output.uri", "mongodb+srv://remote_worker:remote_worker@bddbd.ptwl0.mongodb.net/bigdataproject").\
+    config('spark.jars.packages','org.mongodb.spark:mongo-spark-connector:10.0.0').\
+    config("spark.mongodb.connection.uri","mongodb+srv://remote_worker:remote_worker@bddbd.ptwl0.mongodb.net/bigdataproject").\
+    config("spark.mongodb.database", "bigdataproject").\
+    config("spark.mongodb.collection", "twitter.tweet").\
+    getOrCreate()
 
+# Loead raw
 tweets_raw = spark.read.json("/home/bigdata/datalake/raw/twitter/20220501/*.json")
-users_raw = tweets_raw.select(col('includes.users'))
-users_alone = users_raw.select(explode("users"))
-users_expanded = users_alone.select(col('col.*'))
 
-print(users_expanded.show())
+# Extract Extract
+users = tweets_raw.select(col('includes.users'))\
+    .select(explode("users"))\
+    .select(col('col.*'))\
+    .withColumnRenamed("id","_id")\
+    .dropDuplicates(["_id"])\
+    .drop(col('entities'))
+
+
+print(users.show())
 
 #df3.withColumn("_id", df3.data.id).show()
+
+
+
+
+df = spark.read.format("mongodb").load()
+
 
 
 
