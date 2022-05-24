5f00360102f16d67485a3fb53e6e4a161bada731
MrEthic <jeremiebasso@hotmail.fr>
2022-05-24 18:10:11 +0200

    please

--- src/spark/formating.py
+++ src/spark/formating.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     print(f'Finished at: {t2} | elapsed time {dist.seconds}s')
     #spark.sparkContext._gateway.close()
     spark.stop()
-    exit(0)
+    #exit(0)
 
 #df = spark.read.format("mongodb").load()
 
