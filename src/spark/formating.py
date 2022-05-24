cec3868c178c9c914fe9df3989e424abc7c367de
MrEthic <jeremiebasso@hotmail.fr>
2022-05-24 18:05:49 +0200

    please

--- src/spark/formating.py
+++ src/spark/formating.py
@@ -27,14 +27,10 @@ def datalake_to_mongo():
         .format("mongodb")\
         .mode("append")\
         .option("database","bigdataproject")\
-        .option("collection", "twitter.user")\
-        .save()
+        .option("collection", "twitter.user")
 
     users.show()
 
-    users = spark.createDataFrame([("Bilbo Baggins",  50), ("Gandalf", 1000), ("Thorin", 195), ("Balin", 178), ("Kili", 77),("Dwalin", 169), ("Oin", 167), ("Gloin", 158), ("Fili", 82), ("Bombur", None)], ["name", "age"])
-
-    users.show()
 
 def main():
     datalake_to_mongo()
